22c967ffbee4d81be237a80194a39adcfab2a4d3
James Carpenter <jamesjcarpenter@gmail.com>
2017-11-18 18:48:52 +0000

    requests code tweaks (for reference only)

--- IrcamForum/requests-test/requests-test.py
+++ IrcamForum/requests-test/requests-test.py
@@ -11,19 +11,28 @@ def main():
 
     s = requests.Session()
 
-    login_data = {"username": "UoB", "password": "88iKKmJJa",
-                  "rememberme": "forever",
-                  "redirect_to": "http://forumnet.ircam.fr/",
-                  "redirect_to_automatic": "1"
+    login_data = {"username": "username", "password": "password",
+                  "rememberme": "forever"
+                  # "redirect_to": "http://forumnet.ircam.fr/",
+                  # "redirect_to_automatic": "1"
                   }
+
+
+
 
     page_login = s.post(auth_url, data=login_data)
 
     # modalys_page = s.get('http://forumnet.ircam.fr/product/modalys-en/')
-    audiosculpt_page = s.get('http://forumnet.ircam.fr/shop/en/forumnet/10-audiosculpt.html')
+    # audiosculpt_page = s.get('http://forumnet.ircam.fr/shop/en/forumnet/10-audiosculpt.html')
 
     # print(s.cookies)
-    print(audiosculpt_page.cookies.get_dict())
+    # print(audiosculpt_page.cookies.get_dict())
+
+    # audiosculpt_down = s.get("http://forumnet.ircam.fr/shop/en/get-file.php?id=1735")
+    #
+    # print(audiosculpt_down.text.encode('utf-8'))
+
+    print(page_login.text)
 
     s.close()
 
